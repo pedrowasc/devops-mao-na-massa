e2bb27357a64824309667c04d29ca5e78d16112c
pedrowasc <101606453+pedrowasc@users.noreply.github.com>
2022-05-27 12:55:42 -0300

    sequencia de teste

--- control-node/provision.sh
+++ control-node/provision.sh
@@ -1,6 +1,9 @@
 #/bin/sh
 sudo yum -y install epel-release
 echo "instalando ansible de teste"
+ssh-keygen
+ssh-copy-id vagrant@192.168.0.2
+ssh-copy-id vagrant@192.168.0.4
 sudo yum -y install ansible
 cat <<EOT >> /etc/hosts
 192.168.1.2 control-node
