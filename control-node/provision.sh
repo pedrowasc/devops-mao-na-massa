14b9d7be4a354c982c6406370766b2f9f914e9c5
pedrowasc <101606453+pedrowasc@users.noreply.github.com>
2022-05-28 16:30:54 -0300

    Finalizado organização de base Ansible

--- control-node/provision.sh
+++ control-node/provision.sh
@@ -1,12 +1,21 @@
 #/bin/sh
 sudo yum -y install epel-release
 echo "instalando ansible de teste"
-ssh-keygen
-ssh-copy-id vagrant@192.168.0.2
-ssh-copy-id vagrant@192.168.0.4
 sudo yum -y install ansible
+
 cat <<EOT >> /etc/hosts
-192.168.1.2 control-node
-192.186.1.3 app01
-192.168.1.4 db01
+192.168.10.2 control-node
+192.186.10.3 app01
+192.168.10.4 db01
 EOT
+
+sudo usermod -aG wheel vagrant
+
+sudo cat <<EOT >> /etc/sudoers
+vagrant ALL=(ALL:ALL)  NOPASSWD:ALL
+EOT
+
+ssh-keygen
+
+ssh-copy-id vagrant@192.168.10.3
+ssh-copy-id vagrant@192.168.10.4
--- db01/provision.sh
+++ db01/provision.sh
@@ -1,6 +1,10 @@
 #/bin/sh
-sudo su
-usermod -aG wheel vagrant
+sudo usermod -aG wheel vagrant
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sudo systemctl restart sshd
+sudo cat <<EOT >> /etc/sudoers
+vagrant ALL=(ALL:ALL)  NOPASSWD:ALL
+EOT
 
 #cat <<EOT >> /home/vagrant/.ssh/authorized_keys 
 #ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC730pSsWBCIclSlf4GoToBBh4KqtP8reif+yOlrAq9udQ5UN6ncKP4nyrzWkJ1mmX5DKwxMkwT5ZraxUCWkrvlT5XJlnU0+7Lr0OSy6E0HeiIucQxzSn958B1lC5WSBTTDZSIO3msj3K/46WIn0KyaYFv1WpgeUO2WZMMr0hDeoThMYOHGvJwRzuoVeTQiOF/rpsOM2nnkBNI8OlxQ70q9ZE0ohSqcEC4r/GL55inwMQtw3MfJIJ0D32f9rIecQbB70lsUBT+piaPIwWQsvw+CeeNVrZEGWP324qR+Ni0eUmIrvIJ/3HNqE0EAWiKxcN9l+Or/SzdiFKyjzaTRPtY/ vagrant@control-node 
